3554c62e61745ca7adc68a7605d7aced185750cd
Ethan Bambock <bambock@gmail.com>
2015-02-03 03:04:54 -0800

    renamed the quiz folder which is in .gitignore

--- qz1/dt.R
+++
@@ -1,3 +0,0 @@
-library(data.table)
-DT=data.table(x=rnorm(9), y=rep(c("a","b","c"),each=3), z=rnorm(9))
-head(DT,3)
--- qz1/sh.R
+++
@@ -1,5 +0,0 @@
-library(XML)
-fileUrl<-"http://espn.go.com/nfl/team/_/name/sea/seattle-seahawks"
-doc<-htmlTreeParse(fileUrl, useInternal=TRUE)
-scores<-xpathSApply(doc, "//li[@class='score']", xmlValue)
-teams<-xpathSApply(doc, "//li[@class='team-name']", xmlValue)
